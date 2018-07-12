92e68165599e506d2cbf5fee9485b3b64a64a3d8
Gurdeep-kaur <kaurgurdeep521@gmail.com>
2018-07-12 17:29:16 +0530

    Initial Project Version

---
+++ app.js
@@ -0,0 +1,37 @@
+(function() {
+    'use strict';
+  
+    var app = {
+      isLoading: true,
+      visibleCards: {},
+      spinner: document.querySelector('.loader'),
+      cardTemplate: document.querySelector('.cardTemplate'),
+      container: document.querySelector('.main'),
+      form: document.querySelector('.form')
+    };
+
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker
+                 .register('/service-worker.js')
+                 .then(function() { console.log('Service Worker Registered'); });
+    }
+} )();
+
+var form = document.getElementById('myForm');
+
+form.addEventListener('submit',function validate () {
+    var x = document.forms["myForm"]["email"].value;
+    var atpos = x.indexOf("@");
+    var dotpos = x.lastIndexOf(".");
+    if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length) {
+      alert("Please enter a valid email id")
+    }
+    else {
+        document.forms["myForm"].action = "/dashboard.html";
+    }
+});
+
+document.getElementById('signuplink').addEventListener('click', function () {
+    location.href="./register.html";
+  });
+
---
+++ service-worker.js
@@ -0,0 +1,53 @@
+var cacheName = "cacheDemoapp";
+var filesToCache = [
+  '/',
+  './index.html',
+  './dashboard.html',
+  './register.html',
+  './search.html',
+  './app.js',
+  './styles/style2.css',
+  './styles/style1.css',
+  './styles/small-screen.css',
+  './styles/large-screen.css',
+  './logo.jpg',
+  'https://fonts.googleapis.com/icon?family=Material+Icons'
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+    console.log('[ServiceWorker] Activate');
+    e.waitUntil(
+      caches.keys().then(function(keyList) {
+        return Promise.all(keyList.map(function(key) {
+          if (key !== cacheName) {
+            console.log('[ServiceWorker] Removing old cache', key);
+            return caches.delete(key);
+          }
+        }));
+      })
+    );
+    return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.open(cacheName).then(function(cache) {
+      return cache.match(event.request).then(function (response) {
+        return response || fetch(event.request).then(function(response) {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
